14b4081d691a368ae6c22780bc03e7c2c6f6960e
biangomes <biangomes0707@gmail.com>
2021-02-09 14:22:06 -0500

    new django study

--- projetodjango7/polls/views.py
+++ projetodjango7/polls/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.urls import reverse
+from pip._vendor.certifi.__main__ import args
+
 from .models import Question
 from django.template import loader
 
