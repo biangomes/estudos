0bf2c2fa1620feb3c3dde35c9c0b3109dd8a8ffb
biangomes <biangomes0707@gmail.com>
2021-04-10 22:01:08 -0500

    att

---
+++ python-testes/README.md
@@ -0,0 +1,3 @@
+# Seção 14: Testes e Introdução ao TDD no Python - unittest
+
+Iniciado em: 10/04/21
---
+++ python-testes/main.py
@@ -0,0 +1,7 @@
+from calculadora import soma
+
+# Primeiro tipo de teste
+# print(soma(10, 20))
+# print(soma(10.5, 20))
+
+print(soma(15,15))
