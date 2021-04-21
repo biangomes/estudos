0d79e8c21ed60a813aecaa741252c8c155d178fc
biangomes <biangomes0707@gmail.com>
2021-04-20 19:59:54 -0500

    correcting deploy

--- api/routes.py
+++ api/routes.py
@@ -5,5 +5,7 @@ from api import views
 
 routers = DefaultRouter()
 routers.register("orgs", views.OrganizationViewSet)
+routers.register("orgsdelete", views.OrganizationDelete)
+routers.register("orgsquery", views.OrganizationQuery)
 
 urlpatterns = [path("", include(routers.urls))]
