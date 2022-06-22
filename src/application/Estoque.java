3a60dcf0c4f2796581e235ba4a8407041350d903
biangomes <biangomes0707@gmail.com>
2022-06-22 19:50:58 -0300

    adicionando metodo toString

--- src/application/Estoque.java
+++ src/application/Estoque.java
@@ -22,17 +22,23 @@ public class Estoque {
 
         System.out.printf("Product data: %s, $ %.2f, %d units, Total: $ %.2f\n", p1.name, p1.price, p1.quantity, p1.TotalValueInStock());
 
+
+        // System.out.println(p1); -> ISSO DÁ CERTO POR CAUSA DO MÉTODO TOSTRING
         System.out.println("Adicionar estoque: 1\nRemover estoque: 2\nSair da tela: 3");
         int escolha = input.nextInt();
 
         if (escolha == 1) {
             System.out.println("Quantity: ");
             p1.quantity = input.nextInt();
-            p1.RemoveProducts(p1.quantity);
+            p1.AddProducts(p1.quantity);
+            //p1.UpdateData();
+            System.out.println("Product data: " + p1);
         } else if (escolha == 2) {
             System.out.println("Quantity: ");
             p1.quantity = input.nextInt();
             p1.RemoveProducts(p1.quantity);
+            System.out.println("Product data: " + p1);
+            //p1.UpdateData();
         } else {
             System.exit(0);
         }
--- src/entities/Produto.java
+++ src/entities/Produto.java
@@ -10,13 +10,22 @@ public class Produto {
     }
 
     public void AddProducts(int quantity) {
-        this.quantity += quantity;
-        System.out.printf("Updated data: %s, $ %.2f, %d units, Total: $ %.2f", this.name, this.price, this.quantity, this.TotalValueInStock());
+        this.quantity = this.quantity + quantity;
+        //System.out.printf("Updated data: %s, $ %.2f, %d units, Total: $ %.2f", this.name, this.price, this.quantity, this.TotalValueInStock());
     }
 
     public void RemoveProducts(int quantity) {
-        this.quantity -= quantity;
-        System.out.printf("Updated data: %s, $ %.2f, %d units, Total: $ %.2f", this.name, this.price, this.quantity, this.TotalValueInStock());
+        this.quantity = this.quantity - quantity;
+        //System.out.printf("Updated data: %s, $ %.2f, %d units, Total: $ %.2f", this.name, this.price, this.quantity, this.TotalValueInStock());
     }
+
+    public String toString() {
+        return this.name + ", $ " + this.price + ", " + this.quantity + " units, Total: $ " + this.TotalValueInStock();
+    }
+
+//    public String UpdateData() {
+//        return "Updated data: " + this.name + ", $ " + String.format("%.2f", this.price) + ", " + this.quantity
+//                + "units, Total: $ " + String.format("%.2f",this.TotalValueInStock());
+//    }
 
 }
